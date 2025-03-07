1887d632fe8318ddfe7b3d2092bbe88d2cc0ef64
Piyush Apkaje <apkajepiyush@gmail.com>
2025-03-07 15:37:28 +0530

    Added internal dbt view screen

--- app/src/main/java/in/gov/mahapocra/farmerapppks/ui/screens/dashboard/menugrid/dbt/DbtSchemesDetailsActivity.kt
+++ app/src/main/java/in/gov/mahapocra/farmerapppks/ui/screens/dashboard/menugrid/dbt/DbtSchemesDetailsActivity.kt
@@ -1,12 +1,70 @@
 package `in`.gov.mahapocra.farmerapppks.ui.screens.dashboard.menugrid.dbt
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import `in`.gov.mahapocra.farmerapppks.R
+import org.json.JSONObject
 
 class DbtSchemesDetailsActivity : AppCompatActivity() {
+
+    private lateinit var textViewHeaderTitle: TextView
+    private lateinit var imageMenushow: ImageView
+    private lateinit var importantDocumentsRecyclerView: RecyclerView
+    private lateinit var eligibilityCriteriaRecyclerView: RecyclerView
+    private lateinit var importantDocumentsCardTV: TextView
+    private lateinit var eligibilityCriteriaCardTV: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dbt_schemes_details)
+
+
+        textViewHeaderTitle = findViewById(R.id.textViewHeaderTitle)
+        imageMenushow = findViewById(R.id.imageMenushow)
+        importantDocumentsRecyclerView = findViewById(R.id.importantDocumentsRecyclerView)
+        eligibilityCriteriaRecyclerView = findViewById(R.id.eligibilityCriteriaRecyclerView)
+        importantDocumentsCardTV = findViewById(R.id.importantDocumentsCardTV)
+        eligibilityCriteriaCardTV = findViewById(R.id.eligibilityCriteriaCardTV)
+
+        imageMenushow.visibility = View.VISIBLE
+        textViewHeaderTitle.setText(R.string.dbtschema)
+        imageMenushow.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        val data = intent.getStringExtra("FARMERDBTRESPONSE")
+        val jsonData = JSONObject(data);
+        val importantDocuments = jsonData.optString("importantDocuments")?:jsonData.optString("documents_required")
+        val eligibilityCriteria = jsonData.optString("eligibilityCriteria")?:jsonData.optString("eligibility_criteria")
+        val importantDocumentsArray = importantDocuments.split(";")
+        val eligibilityCriteriaArray = eligibilityCriteria.split(";")
+
+        importantDocumentsCardTV.setOnClickListener {
+            openRecyclerView(importantDocumentsRecyclerView)
+        }
+
+        eligibilityCriteriaCardTV.setOnClickListener {
+            openRecyclerView(eligibilityCriteriaRecyclerView)
+        }
+
+        importantDocumentsRecyclerView.layoutManager = LinearLayoutManager(this)
+        importantDocumentsRecyclerView.adapter = FarmerDbtSchemeDetailsRecyclerAdapter(importantDocumentsArray)
+
+        eligibilityCriteriaRecyclerView.layoutManager = LinearLayoutManager(this)
+        eligibilityCriteriaRecyclerView.adapter = FarmerDbtSchemeDetailsRecyclerAdapter(eligibilityCriteriaArray)
+    }
+
+    private fun openRecyclerView(recyclerView: RecyclerView) {
+        if (recyclerView.visibility==View.VISIBLE) {
+            recyclerView.visibility = View.GONE
+        }else{
+            recyclerView.visibility = View.VISIBLE
+        }
     }
 }
