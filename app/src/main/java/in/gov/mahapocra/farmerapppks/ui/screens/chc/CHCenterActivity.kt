4d22c880689ea46841f4ffb9a0daa4c1e48feb3b
Piyush Apkaje <apkajepiyush@gmail.com>
2025-03-17 11:58:51 +0530

    Adjusted colors for the default setting.

--- app/src/main/java/in/gov/mahapocra/farmerapppks/ui/screens/chc/CHCenterActivity.kt
+++ app/src/main/java/in/gov/mahapocra/farmerapppks/ui/screens/chc/CHCenterActivity.kt
@@ -1,9 +1,12 @@
 package `in`.gov.mahapocra.farmerapppks.ui.screens.chc
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import `in`.gov.mahapocra.farmerapppks.R
 import `in`.gov.mahapocra.farmerapppks.databinding.ActivityChcenterBinding
 
 class CHCenterActivity : AppCompatActivity() {
@@ -25,6 +28,20 @@ class CHCenterActivity : AppCompatActivity() {
 
         for(i in 1..10){
             tempStrArr.add("Hello $i")
+        }
+        binding.previousSevenDayTV.apply {
+            background =
+                ContextCompat.getDrawable(
+                    this@CHCenterActivity,
+                    R.drawable.shape_right_green
+                )
+            setTextColor(Color.WHITE)
+        }
+
+        binding.nextSevenDayTV.apply {
+            background =
+                ContextCompat.getDrawable(this@CHCenterActivity, R.drawable.shape_left_white)
+            setTextColor(Color.BLACK)
         }
 
         adapter = CHCenterRecyclerAdapter(tempStrArr)
