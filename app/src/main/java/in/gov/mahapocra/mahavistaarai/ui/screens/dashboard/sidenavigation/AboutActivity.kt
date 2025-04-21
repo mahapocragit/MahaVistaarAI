607cf8ef5674065ea502ad42b7d5d9bce0bc5f99
Piyush Apkaje <apkajepiyush@gmail.com>
2025-04-21 17:27:57 +0530

    Added about section for mahaVistaar.

--- app/src/main/java/in/gov/mahapocra/mahavistaarai/ui/screens/dashboard/sidenavigation/AboutActivity.kt
+++ app/src/main/java/in/gov/mahapocra/mahavistaarai/ui/screens/dashboard/sidenavigation/AboutActivity.kt
@@ -20,5 +20,9 @@ class AboutActivity : AppCompatActivity() {
             onBackPressedDispatcher.onBackPressed()
         }
         binding.toolbarLayout.textViewHeaderTitle.text = getString(R.string.about)
+
+        binding.textView13.text =
+            "Welcome to the MahaVISTAAR-AI App, your trusted companion in modern farming at your fingertips, from the Department of Agriculture, Government of Maharashtra.\n" +
+                    "Farmer App is the one-stop solution for farmers which is focused on empowering farmers by providing essential agricultural information, real-time weather updates, market prices, pest identification and control tips, agromet advisories, warehouse and CHC information, expert advice to help farmers make informed decisions and many more.  By integrating technology with climate-resilient farming practices, we aim to promote sustainable and profitable agriculture. From monitoring your crops to connecting with fellow farmers, we ensure you’re never alone on your farming journey. Let’s grow together!"
     }
 }
