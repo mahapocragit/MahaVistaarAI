6f319fb01ac4a3a92224b3d638039d3804ae0d13
Piyush Apkaje <apkajepiyush@gmail.com>
2025-05-05 14:05:49 +0530

    Added launcher icons added values for soil health card.

--- app/src/main/java/in/gov/mahapocra/mahavistaarai/ui/screens/dashboard/menugrid/soilhealthcard/SoilHealthCardAdapter.kt
+++ app/src/main/java/in/gov/mahapocra/mahavistaarai/ui/screens/dashboard/menugrid/soilhealthcard/SoilHealthCardAdapter.kt
@@ -23,6 +23,10 @@ class SoilHealthCardAdapter(private val farmerJsonArray: JSONArray?) :
         val nitrogenImageView: ImageView = itemView.findViewById(R.id.nitrogenImageView)
         val phosphorusImageView: ImageView = itemView.findViewById(R.id.phosphorusImageView)
         val potassiumImageView: ImageView = itemView.findViewById(R.id.potassiumImageView)
+
+        val ecImageView: ImageView = itemView.findViewById(R.id.ecImageView)
+        val ocImageView: ImageView = itemView.findViewById(R.id.ocImageView)
+        val phImageView: ImageView = itemView.findViewById(R.id.phImageView)
         val soilHealthReportLinearLayout: LinearLayout =
             itemView.findViewById(R.id.soilHealthReportLinearLayout)
     }
@@ -36,12 +40,14 @@ class SoilHealthCardAdapter(private val farmerJsonArray: JSONArray?) :
     override fun onBindViewHolder(holder: FarmerViewHolder, position: Int) {
         farmerJsonArray?.let {
             val farmerObject: JSONObject = it.getJSONObject(position)
-            val npkUnfiltered = farmerObject.optString("npk")
-            val npkArray = npkUnfiltered.split("/")
-            val nitrogen = npkArray[0]
-            val phosphorus = npkArray[1]
-            val potassium = npkArray[2]
-            mapNPKByColors(nitrogen, phosphorus, potassium, holder)
+            val npkUnfiltered = farmerObject.optJSONObject("soil_params")
+            val nitrogen = npkUnfiltered.getString("n")
+            val phosphorus = npkUnfiltered.getString("p")
+            val potassium = npkUnfiltered.getString("k")
+            val electricalConductivity = npkUnfiltered.getString("ec")
+            val organicCarbon = npkUnfiltered.getString("oc")
+            val potentialOfHydrogen = npkUnfiltered.getString("ph")
+            mapNPKByColors(nitrogen, phosphorus, potassium, electricalConductivity, organicCarbon, potentialOfHydrogen, holder)
             holder.farmerName.text = farmerObject.optString("farmer_name", "N/A")
             holder.farmSize.text = "${farmerObject.optString("farmsize", "0")} acres"
             holder.shcNo.text = "${farmerObject.optString("shc_no", "N/A")}"
@@ -64,11 +70,17 @@ class SoilHealthCardAdapter(private val farmerJsonArray: JSONArray?) :
         nitrogen: String,
         phosphorus: String,
         potassium: String,
+        electricalConductivity: String,
+        organicCarbon: String,
+        potentialOfHydrogen: String,
         holder: FarmerViewHolder
     ) {
         val nitroDouble = nitrogen.toDouble()
         val phosDouble = phosphorus.toDouble()
         val potasDouble = potassium.toDouble()
+        val electricalConductivityDouble = electricalConductivity.toDouble()
+        val organicCarbonDouble = organicCarbon.toDouble()
+        val potentialOfHydrogenDouble = potentialOfHydrogen.toDouble()
 
 
         if (nitroDouble < 280) {
@@ -93,6 +105,30 @@ class SoilHealthCardAdapter(private val farmerJsonArray: JSONArray?) :
             holder.potassiumImageView.setImageResource(R.drawable.ic_soil_health_card_low)
         }else{
             holder.potassiumImageView.setImageResource(R.drawable.ic_soil_health_card_mid)
+        }
+
+        if (electricalConductivityDouble < 2) {
+            holder.ecImageView.setImageResource(R.drawable.ic_soil_health_card_high)
+        } else if (electricalConductivityDouble > 2 && electricalConductivityDouble < 4){
+            holder.ecImageView.setImageResource(R.drawable.ic_soil_health_card_low)
+        }else{
+            holder.ecImageView.setImageResource(R.drawable.ic_soil_health_card_mid)
+        }
+
+        if (organicCarbonDouble < 0.4) {
+            holder.ocImageView.setImageResource(R.drawable.ic_soil_health_card_high)
+        } else if (organicCarbonDouble > 0.4 && organicCarbonDouble < 0.8){
+            holder.ocImageView.setImageResource(R.drawable.ic_soil_health_card_low)
+        }else{
+            holder.ocImageView.setImageResource(R.drawable.ic_soil_health_card_mid)
+        }
+
+        if (potentialOfHydrogenDouble < 6.5) {
+            holder.phImageView.setImageResource(R.drawable.ic_soil_health_card_high)
+        } else if (potentialOfHydrogenDouble > 6.5 && potentialOfHydrogenDouble < 7.5){
+            holder.phImageView.setImageResource(R.drawable.ic_soil_health_card_low)
+        }else{
+            holder.phImageView.setImageResource(R.drawable.ic_soil_health_card_mid)
         }
     }
 
