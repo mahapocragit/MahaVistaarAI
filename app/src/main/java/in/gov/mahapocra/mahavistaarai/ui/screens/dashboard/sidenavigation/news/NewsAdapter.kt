d1ccdad037b3c11d3b3fc0ce6f30ea062a8c99ec
Piyush Apkaje <apkajepiyush@gmail.com>
2025-05-15 18:16:46 +0530

    Added new logo for bot and date field in news

--- app/src/main/java/in/gov/mahapocra/mahavistaarai/ui/screens/dashboard/sidenavigation/news/NewsAdapter.kt
+++ app/src/main/java/in/gov/mahapocra/mahavistaarai/ui/screens/dashboard/sidenavigation/news/NewsAdapter.kt
@@ -7,6 +7,7 @@ import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,6 +20,7 @@ class NewsAdapter(val jsonArray: JSONArray, val language: String) :
     RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val newsEventTitleTextView: TextView = view.findViewById(R.id.newsEventTitleTextView)
+        val dateTextView: TextView = view.findViewById(R.id.dateTextView)
         val newsEventDescriptionTextView: TextView =
             view.findViewById(R.id.newsEventDescriptionTextView)
     }
@@ -34,6 +36,8 @@ class NewsAdapter(val jsonArray: JSONArray, val language: String) :
         val newsUrlToLoad = jsonObject.optString("link")
         val englishTitle = jsonObject.optString("news_headline")
         val englishDescription = jsonObject.optString("short_description")
+        val date = jsonObject.optString("date")
+        Log.d("TAGGER", "onBindViewHolder: ${formatDate(date)}")
 
         val translationsJsonObject = jsonObject.optJSONObject("translations")
         val marathiJsonObject = translationsJsonObject?.optJSONObject("mr")
@@ -44,6 +48,11 @@ class NewsAdapter(val jsonArray: JSONArray, val language: String) :
         val title = if (language == "mr") marathiTitle else englishTitle
 
         holder.newsEventTitleTextView.text = title
+        holder.dateTextView.text = buildString {
+            append(holder.dateTextView.context.getString(R.string.date))
+            append("  ")
+            append(formatDate(date))
+        }
 
         // Set up expand/collapse logic
         val words = fullDescription?.split(" ") ?: emptyList()
@@ -90,9 +99,8 @@ class NewsAdapter(val jsonArray: JSONArray, val language: String) :
         isExpanded: Boolean
     ): SpannableString {
         val suffix =
-            if (isExpanded) holder.newsEventTitleTextView.context.getString(R.string.read_more) else holder.newsEventTitleTextView.context.getString(
-                R.string.read_more
-            )
+            if (isExpanded) holder.newsEventTitleTextView.context.getString(R.string.read_less)
+            else holder.newsEventTitleTextView.context.getString(R.string.read_more)
         val displayText = if (isExpanded) fullText + suffix else previewText + suffix
         val spannable = SpannableString(displayText)
 
@@ -113,5 +121,9 @@ class NewsAdapter(val jsonArray: JSONArray, val language: String) :
         )
 
         return spannable
+    }
+
+    fun formatDate(dateTime: String): String {
+        return dateTime.substringBefore("T")
     }
 }
