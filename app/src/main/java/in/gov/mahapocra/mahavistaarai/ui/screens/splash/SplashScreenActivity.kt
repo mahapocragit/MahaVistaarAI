874d50cd5e5baf9b6d01341c1185830c93bfb465
Piyush Apkaje <apkajepiyush@gmail.com>
2025-09-29 13:43:08 +0530

    Version 1.9.4: Alpha Added clarity access for stage and prod. and added two separate json for firebase connectivity.

--- app/src/main/java/in/gov/mahapocra/mahavistaarai/ui/screens/splash/SplashScreenActivity.kt
+++ app/src/main/java/in/gov/mahapocra/mahavistaarai/ui/screens/splash/SplashScreenActivity.kt
@@ -13,6 +13,7 @@ import com.microsoft.clarity.ClarityConfig
 import com.microsoft.clarity.models.LogLevel
 import `in`.co.appinventor.services_api.settings.AppSettings
 import `in`.gov.mahapocra.mahavistaarai.R
+import `in`.gov.mahapocra.mahavistaarai.data.api.ApiConstants
 import `in`.gov.mahapocra.mahavistaarai.ui.screens.authentication.LoginScreen
 import `in`.gov.mahapocra.mahavistaarai.ui.screens.dashboard.menugrid.DashboardScreen
 import `in`.gov.mahapocra.mahavistaarai.util.LocalCustom
@@ -37,7 +38,7 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         val config = ClarityConfig(
-            projectId = "rqkkntubvb",
+            projectId = ApiConstants.clarityKey,
             logLevel = LogLevel.Verbose
         )
         Clarity.initialize(applicationContext, config)
