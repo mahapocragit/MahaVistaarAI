1ef9db382069cc62386e9a1300922bb5e1c977e1
Piyush Apkaje <apkajepiyush@gmail.com>
2026-09-09 18:13:56 +0530

    MahaVISTAAR-AI: 2.1.5 Alpha - Added ui for weather and advisory screen.

--- app/src/main/java/in/gov/mahapocra/mahavistaarai/ui/screens/newui/farmdetails/fragments/FarmObservationFragment.kt
+++ app/src/main/java/in/gov/mahapocra/mahavistaarai/ui/screens/newui/farmdetails/fragments/FarmObservationFragment.kt
@@ -5,11 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mahanidan.vcccore.common.i18n.NativeUiConfig
 import com.mahanidan.vcccore.my_farms.view.MyFarmsUi
 import `in`.gov.mahapocra.mahavistaarai.R
+import `in`.gov.mahapocra.mahavistaarai.ui.screens.newui.farmdetails.adapters.AdvisoryAdapter
+import `in`.gov.mahapocra.mahavistaarai.ui.screens.newui.farmdetails.adapters.AdvisoryModel
+import `in`.gov.mahapocra.mahavistaarai.ui.screens.newui.farmdetails.adapters.WeatherFarmAdapter
+import `in`.gov.mahapocra.mahavistaarai.ui.screens.newui.farmdetails.adapters.WeatherFarmModel
 
 class FarmObservationFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var weatherFarmAdapter: WeatherFarmAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +30,39 @@ class FarmObservationFragment : Fragment() {
             container,
             false
         )
+
+        recyclerView = view.findViewById(R.id.weatherFarmRecyclerView)
+        val weatherList = List(24) { hour ->
+            val displayHour = when {
+                hour == 0 -> 12
+                hour > 12 -> hour - 12
+                else -> hour
+            }
+
+            val amPm = if (hour < 12) "AM" else "PM"
+
+            WeatherFarmModel(
+                rainfall = "${(0..100).random()}%",
+                time = String.format("%02d:00 %s", displayHour, amPm),
+                humidity = "${(40..95).random()}%",
+                wind = "${(5..30).random()} km/r"
+            )
+        }
+
+        weatherFarmAdapter = WeatherFarmAdapter(
+            weatherList
+        ) { selectedItem ->
+
+            // Item clicked
+//            submitSelectedItem(selectedItem)
+        }
+
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = weatherFarmAdapter
+            setHasFixedSize(true)
+        }
+
         return view
     }
 }
